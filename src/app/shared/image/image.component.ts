c4c9f8bba18f76d94529c3f4e8a7a44c53801a62
Jesus Ospino <lancero543@gmail.com>
2025-07-24 09:46:18 -0500

    feat: se crea componente imagen

---
+++ src/app/shared/image/image.component.ts
@@ -0,0 +1,23 @@
+import { Component, input, output } from '@angular/core';
+import { ImageModule } from 'primeng/image';
+
+@Component({
+  selector: 'app-image',
+  imports: [ImageModule],
+  templateUrl: './image.component.html',
+  styleUrl: './image.component.scss',
+})
+export class ImageComponent {
+  width = input<string>('200');
+  height = input<string>('');
+  src = input<string>();
+  alt = input<string>();
+  preview = input<boolean>(false);
+  showRemove = input<boolean>(false);
+
+  onRemove = output<Event>();
+
+  remove(event: Event) {
+    this.onRemove.emit(event);
+  }
+}
