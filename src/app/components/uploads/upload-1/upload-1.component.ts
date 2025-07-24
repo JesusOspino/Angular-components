3571300e776179cd3ec59209809b17def668503d
Jesus Ospino <lancero543@gmail.com>
2025-07-24 09:46:58 -0500

    feat: Se añade primer componente para subida de imagenes

--- src/app/components/uploads/upload-1/upload-1.component.ts
+++ src/app/components/uploads/upload-1/upload-1.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ImageComponent } from 'src/app/shared/image/image.component';
 
 @Component({
   selector: 'app-upload-1',
-  imports: [],
+  imports: [FileUploadModule, ButtonModule, ImageComponent],
   templateUrl: './upload-1.component.html',
-  styleUrl: './upload-1.component.scss'
+  styleUrl: './upload-1.component.scss',
 })
 export class Upload1Component {
+  selectedFiles = signal<File[]>([]);
 
+  /**
+   * Selecciona la imagen del bordado
+   * @param event
+   */
+  public async onFileSelect(event: { currentFiles: File[] }) {
+    const files = event.currentFiles;
+    if (files && files.length > 0) {
+      // Actualizamos la señal con el archivo seleccionado
+      this.selectedFiles.set(files);
+    }
+  }
 }
--- src/app/components/uploads/uploads.component.ts
+++ src/app/components/uploads/uploads.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ConfigPageService } from 'src/app/common/services/config-page.service';
+import { Upload1Component } from './upload-1/upload-1.component';
+import { Upload2Component } from './upload-2/upload-2.component';
 
 @Component({
   selector: 'app-uploads',
-  imports: [],
+  imports: [Upload1Component, Upload2Component],
   templateUrl: './uploads.component.html',
-  styleUrl: './uploads.component.scss'
+  styleUrl: './uploads.component.scss',
 })
 export class UploadsComponent {
+  private readonly configPageService = inject(ConfigPageService);
 
+  ngOnInit(): void {
+    this.configPage();
+  }
+
+  configPage() {
+    this.configPageService.setTitle('Uploads');
+    this.configPageService.setDescription(
+      'Componentes para subida de imagenes o archivos'
+    );
+    this.configPageService.setBreadcrumbItems([
+      { label: 'Components' },
+      { label: 'Uploads' },
+    ]);
+  }
 }
