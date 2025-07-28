320a534579244411c31ed960192878addd38c9c9
Jesus Ospino <lancero543@gmail.com>
2025-07-28 09:26:22 -0500

    fix: Se ajustan estilos de views

--- src/app/components/views/view-3/view-3.component.ts
+++ src/app/components/views/view-3/view-3.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { ContentComponent } from 'src/app/shared/content/content.component';
 
 @Component({
   selector: 'app-view-3',
-  imports: [ContentComponent],
+  imports: [ContentComponent, ButtonModule],
   templateUrl: './view-3.component.html',
   styleUrl: './view-3.component.scss',
 })
-export class View3Component {}
+export class View3Component {
+  accept() {}
+  cancel() {}
+}
