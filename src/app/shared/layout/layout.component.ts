17603e361ca1c2c80bd254363a86d04cb8347738
Jesus Ospino <lancero543@gmail.com>
2025-08-01 09:13:22 -0500

    feat: Se pone splitter en layout para separar contenido y menu

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeConfiguratorButtonComponent } from '@shared/theme-configurator-button/theme-configurator-button.component';
 import { ThemeToggleButtonComponent } from '@shared/theme-toggle-button/theme-toggle-button.component';
+import { SplitterModule } from 'primeng/splitter';
 
 @Component({
   selector: 'app-layout',
@@ -9,8 +11,16 @@ import { ThemeToggleButtonComponent } from '@shared/theme-toggle-button/theme-to
     RouterOutlet,
     ThemeToggleButtonComponent,
     ThemeConfiguratorButtonComponent,
+    SplitterModule,
+    NgClass,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  menuVisible = signal<boolean>(false);
+
+  toggleMenu() {
+    this.menuVisible.update((visible) => !visible);
+  }
+}
