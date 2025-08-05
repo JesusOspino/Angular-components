25fdea7fad711004f3ff8db3bbd88302bfc252e3
Jesus Ospino <lancero543@gmail.com>
2025-08-05 08:43:02 -0500

    fix: Se quitar por completo estructura con splitter

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,9 +1,7 @@
-import { CommonModule, NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeConfiguratorButtonComponent } from '@shared/theme-configurator-button/theme-configurator-button.component';
 import { ThemeToggleButtonComponent } from '@shared/theme-toggle-button/theme-toggle-button.component';
-import { SplitterModule } from 'primeng/splitter';
 
 @Component({
   selector: 'app-layout',
@@ -11,16 +9,8 @@ import { SplitterModule } from 'primeng/splitter';
     RouterOutlet,
     ThemeToggleButtonComponent,
     ThemeConfiguratorButtonComponent,
-    SplitterModule,
-    NgClass,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
-  menuVisible = signal<boolean>(false);
-
-  toggleMenu() {
-    this.menuVisible.update((visible) => !visible);
-  }
-}
+export class LayoutComponent {}
